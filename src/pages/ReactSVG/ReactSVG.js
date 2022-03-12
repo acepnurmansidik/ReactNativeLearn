0f395d1c7e035d669067c84602b5fa40c288f6cb
acepnurmansidik <acepnurmansidik@gmail.com>
2022-03-13 06:14:18 +0700

    styling page

--- src/pages/ReactSVG/ReactSVG.js
+++ src/pages/ReactSVG/ReactSVG.js
@@ -1,20 +1,32 @@
 /* eslint-disable prettier/prettier */
-import {View, Text, StyleSheet,Image} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 import React from 'react';
-import NotFound from '../../assets/images/undraw_my_app_re_gxtj.svg'
+import NotFound from '../../assets/images/undraw_my_app_re_gxtj.svg';
 
 export default function ReactSVG() {
   return (
-    <View>
+    <View style={styles.container}>
       <Text style={styles.header}>React Native SVG</Text>
-      <NotFound width={`100%`} height={200} />
+      <NotFound width={200} height={200} />
+      <View style={styles.line} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container:{
+    display:'flex',
+    justifyContent:'center',
+    alignItems:'center',
+  },
   header: {
     textAlign: 'center',
-    marginBottom:20
+    marginBottom:20,
+    fontSize:20,
+  },
+  line:{
+    width:'95%',
+    height:2,
+    backgroundColor:'#000',
   },
 });
