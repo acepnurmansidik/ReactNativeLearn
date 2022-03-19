a7b41f4728f1a2d277e2bb7248e0b16865df60a0
acepnurmansidik <acepnurmansidik@gmail.com>
2022-03-19 17:11:40 +0700

    Push Local Notification

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
+/* eslint-disable no-unused-vars */
 import {ScrollView, View, StyleSheet} from 'react-native';
 import React from 'react';
 import ReactSVG from './src/pages/ReactSVG/ReactSVG';
 import CallAPIAxios from './src/pages/CallAPIAxios/CallAPIAxios';
 import LocalAPI from './src/pages/LocalAPI/LocalAPI';
+import PushLocalNotification from './src/pages/PushLocalNotification/PushLocalNotification';
 
 export default function App() {
   return (
@@ -10,7 +12,8 @@ export default function App() {
       <ScrollView>
         {/* <ReactSVG /> */}
         {/* <CallAPIAxios /> */}
-        <LocalAPI />
+        {/* <LocalAPI /> */}
+        <PushLocalNotification />
       </ScrollView>
     </View>
   );
---
+++ src/pages/PushLocalNotification/PushLocalNotification.js
@@ -0,0 +1,190 @@
+import React, {useState} from 'react';
+import {
+  TextInput,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Alert,
+} from 'react-native';
+import NotifService from '../../../NotifService';
+
+export default function PushLocalNotification() {
+  const [registerToken, setRegisterToken] = useState('');
+  const [fcmRegistered, setFcmRegister] = useState(false);
+
+  const onRegister = token => {
+    setRegisterToken(token.token);
+    setFcmRegister(true);
+  };
+
+  const onNotif = notif => {
+    Alert.alert(notif.title, notif.message);
+  };
+
+  const notif = new NotifService(onRegister, onNotif);
+
+  const handlePerm = perms => {
+    Alert.alert('Permissions', JSON.stringify(perms));
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.line} />
+      <Text style={styles.title}>
+        Example app react-native-push-notification
+      </Text>
+      <View style={styles.spacer} />
+      <TextInput
+        style={styles.textField}
+        value={registerToken}
+        placeholder="Register token"
+      />
+      <View style={styles.spacer} />
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          notif.localNotif();
+        }}>
+        <Text>Local Notification (now)</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          notif.localNotif('sample.mp3');
+        }}>
+        <Text>Local Notification with sound (now)</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          notif.scheduleNotif();
+        }}>
+        <Text>Schedule Notification in 5s</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          notif.scheduleNotif('sample.mp3');
+        }}>
+        <Text>Schedule Notification with sound in 30s</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          notif.cancelNotif();
+        }}>
+        <Text>Cancel last notification (if any)</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          notif.cancelAll();
+        }}>
+        <Text>Cancel all notifications</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          notif.checkPermission(handlePerm.bind(this));
+        }}>
+        <Text>Check Permission</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          notif.requestPermissions();
+        }}>
+        <Text>Request Permissions</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          notif.abandonPermissions();
+        }}>
+        <Text>Abandon Permissions</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          notif.getScheduledLocalNotifications(notifs => console.log(notifs));
+        }}>
+        <Text>Console.Log Scheduled Local Notifications</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          notif.getDeliveredNotifications(notifs => console.log(notifs));
+        }}>
+        <Text>Console.Log Delivered Notifications</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          notif.createOrUpdateChannel();
+        }}>
+        <Text>Create or update a channel</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          notif.popInitialNotification();
+        }}>
+        <Text>popInitialNotification</Text>
+      </TouchableOpacity>
+
+      <View style={styles.spacer} />
+
+      {fcmRegistered && <Text>FCM Configured !</Text>}
+
+      <View style={styles.spacer} />
+      <View style={styles.line} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  button: {
+    borderWidth: 1,
+    borderColor: '#000000',
+    margin: 5,
+    padding: 5,
+    width: '70%',
+    backgroundColor: '#DDDDDD',
+    borderRadius: 5,
+  },
+  textField: {
+    borderWidth: 1,
+    borderColor: '#AAAAAA',
+    margin: 5,
+    padding: 5,
+    width: '70%',
+  },
+  spacer: {
+    height: 10,
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    textAlign: 'center',
+  },
+  line: {
+    width: '95%',
+    height: 2,
+    backgroundColor: '#000',
+    marginTop: 20,
+    marginBottom: 20,
+  },
+});
